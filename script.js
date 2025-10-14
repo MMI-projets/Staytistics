ebf92c1d8b6b025a017ac8e8ed3b107535eff770
allaxan <158027244+allaxan@users.noreply.github.com>
2025-10-14 14:31:22 +0000

    ajout de la fonctionnalité de mettre sur pause la vidéo

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
+
+
+   const playButtons = document.querySelectorAll(".play");
+    playButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const video = button.previousElementSibling;
+            if (video.paused) {
+                video.play();
+                button.textContent = "⏸"; // Change to pause icon
+            } else {
+                video.pause();
+                button.textContent = "⏵"; // Change to play icon
+            }
+        });
+    });
 });
